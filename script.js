cc6709e57128ca28a8952494abc3abb702372635
Kiril Bogza <sobacika1999@gmail.com>
2025-10-08 11:47:31 +0200

    1

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
+const boxExplore = document.getElementById("boxExplore")
+
 // API
 
-const API_KEY = "8ad64df3c0mshd5b7a83a018b82dp1d79bajsn9530cc086a66"
-const API_URL = "https://www.dbooks.org/api/recent";
-
-const containerExplore = document.getElementById("boxExplore");
-const carousel1 = document.getElementById("carousel-1");
-const carousel2 = document.getElementById("carousel-2");
-const carousel3 = document.getElementById("carousel-3");
+const API_KEY = "8ad64df3c0mshd5b7a83a018b82dp1d79bajsn9530cc086a66";
+const API_URL =
+  "https://project-gutenberg-free-books-api1.p.rapidapi.com/books";
 
 const getBooks = async () => {
   const response = await fetch(API_URL, {
     headers: {
-      "x-rapidapi-key": API_KEY
-    }
+      "x-rapidapi-key": API_KEY,
+    },
   });
 
   const parsedResponse = await response.json();
 
-  const books = parsedResponse.books;
+  const books = parsedResponse.results;
 
   console.log(books);
 
   return books;
 };
 
-const displayBooksExplore = async () => { //Randomize
+getBooks()
+
+const getBooksExplore = async () => {
+  //Randomize
   const books = await getBooks();
 
   for (const book of books) {
     const title = book.title;
-    const image = book.image;
+    const image = book.cover_image;
 
     const bookContainer = document.createElement("div");
 
@@ -40,15 +41,17 @@ const displayBooksExplore = async () => { //Randomize
 
     bookContainer.appendChild(bookImage);
     bookContainer.appendChild(bookTitle);
-    containerExplore.appendChild(bookContainer);
+    boxExplore.appendChild(bookContainer);
   }
 };
+
+getBooksExplore()
 
 const displayBooksExploreCarousel = async () => {
   const books = await getBooks();
 
-  let books1 = []
-  let books2
+  let books1 = [];
+  let books2;
 
   for (let i = 0; i < 3 && i < books.length; i++) {
     const book = books[i];
@@ -56,7 +59,7 @@ const displayBooksExploreCarousel = async () => {
 
     const bookContainer = document.createElement("div");
     const bookImage = document.createElement("img");
-    
+
     bookImage.src = image;
     bookContainer.classList.add("item");
     bookContainer.appendChild(bookImage);
@@ -65,43 +68,29 @@ const displayBooksExploreCarousel = async () => {
     carousel2.appendChild(bookContainer.cloneNode(true));
     carousel3.appendChild(bookContainer.cloneNode(true));
   }
-
 };
 
 // Buttons
 
 const nextDiv = () => {
-  const carousel = document.querySelector('.container-carousel');
-  carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
+  const carousel = document.querySelector(".container-carousel");
+  carousel.scrollBy({ left: carousel.offsetWidth, behavior: "smooth" });
 };
 
 const previousDiv = () => {
-  const carousel = document.querySelector('.container-carousel');
-  carousel.scrollBy({ left: -carousel.offsetWidth, behavior: 'smooth' });
+  const carousel = document.querySelector(".container-carousel");
+  carousel.scrollBy({ left: -carousel.offsetWidth, behavior: "smooth" });
 };
 
 const scrollLeftBooks = () => {
-  containerExplore.scrollBy({ left: -300, behavior: "smooth" });
+  boxExplore.scrollBy({ left: -300, behavior: "smooth" });
 };
 
 const scrollRightBooks = () => {
-  containerExplore.scrollBy({ left: 300, behavior: "smooth" });
+  boxExplore.scrollBy({ left: 300, behavior: "smooth" });
 };
 
 // Other functionality
 
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
-
-displayBooksExplore();
-displayBooksExploreCarousel();
-
-
-
-
-
-
-
-
-
-
